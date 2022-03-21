b1ce139db8457cec07b89b899cdca1abf3516d17
Corey M <corey@codenation.com>
2022-03-22 09:40:29 +1000

    Refactor the CodeEditor dynamic imports
    This is necessary for the code to work on Netlify

--- utils/plugins.jsx
+++ utils/plugins.jsx
@@ -3,7 +3,18 @@
 // > ReferenceError: navigator is not defined #77
 // https://github.com/JedWatson/react-codemirror/issues/77
 import dynamic from "next/dynamic";
-const CodeEditor = dynamic(import("./CodeEditor.tsx"), { ssr: false });
+const CodeEditor = dynamic(
+  dynamic(() => {
+    // Load all the JS dependencies for the code editor
+    import("codemirror/mode/htmlmixed/htmlmixed");
+    import("codemirror/keymap/sublime.js");
+    import("codemirror/addon/edit/matchbrackets.js");
+    import("codemirror/addon/edit/closebrackets.js");
+    // Return the CodeEditor component itself
+    return import("./CodeEditor.tsx");
+  }),
+  { ssr: false }
+);
 
 export const CodeEditorPlugin = {
   __type: "field",
